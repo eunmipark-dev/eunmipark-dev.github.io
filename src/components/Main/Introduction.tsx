84920a95017d5645071c5b2ab2165947d61ff0bd
eunmi <pomvvv@gmail.com>
2025-02-05 14:33:42 +0900

    init

---
+++ src/components/Main/Introduction.tsx
@@ -0,0 +1,70 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
+import ProfileImage from 'components/Main/ProfileImage'
+
+type IntroductionProps = {
+  profileImage: IGatsbyImageData
+}
+
+const Background = styled.div`
+  width: 100%;
+  /*background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);*/
+  color: #00000;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 768px;
+  height: 400px;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 300px;
+    padding: 0 20px;
+  }
+`
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
+`
+
+const Title = styled.div`
+  margin-top: 5px;
+  font-size: 25px;
+  font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 25px;
+  }
+`
+
+const Introduction: FunctionComponent<IntroductionProps> = function ({
+  profileImage,
+}) {
+  return (
+    <Background>
+      <Wrapper>
+        <ProfileImage profileImage={profileImage} />
+
+        <div>
+          <Title>Hi, I'm Eunmi Park </Title>
+          <SubTitle>
+            a frontend developer specializing in GIS and HD-map.
+          </SubTitle>
+        </div>
+      </Wrapper>
+    </Background>
+  )
+}
+
+export default Introduction
